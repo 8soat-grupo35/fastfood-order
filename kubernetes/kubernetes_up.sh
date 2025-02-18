aec70d33cdc2bf528f4053329103cdcf7b7b57ea
Egio <egio.lima@ifood.com.br>
2025-02-17 22:27:13 -0300

    feat: update configuration for environment variables and add AWS deployment workflow

--- kubernetes/kubernetes_up.sh
+++ kubernetes/kubernetes_up.sh
@@ -1,9 +1,11 @@
 docker build -t fastfood-order-app:latest ../.
-kubectl apply -f ./postgres-dbinit-configmap.yaml
-kubectl apply -f ./postgres-pv.yaml
-kubectl apply -f ./postgres-pvc.yaml
-kubectl apply -f ./postgres-deploy.yaml
-kubectl apply -f ./postgres-service.yaml
-kubectl apply -f ./fastfood-deployment.yaml
-kubectl apply -f ./fastfood-service.yaml
-kubectl apply -f ./fastfood-hpa.yaml
+
+kubectl create secret generic aws-secrets \
+  --from-literal=access-key-id=$(aws configure get aws_access_key_id) \
+  --from-literal=secret-access-key=$(aws configure get aws_secret_access_key) \
+  --from-literal=access-session-token=$(aws configure get aws_session_token)
+
+kubectl apply -f ./fastfood-order-secrets.yaml
+kubectl apply -f ./fastfood-order-deployment.yaml
+kubectl apply -f ./fastfood-order-service.yaml
+kubectl apply -f ./fastfood-order-hpa.yaml
